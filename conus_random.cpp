c8c4da16c8ba0b42cd5081fb2d68765e2cd5f8d5
Johannes Blaschke <jpblaschke@lbl.gov>
2020-03-04 17:55:43 -0800

    first implementation of a GPU version of a uniform variate class that's 'compatible' with GalSim

--- conus_random.cpp
+++ conus_random.cpp
@@ -6,7 +6,8 @@
 
 ConusUniformCPU::ConusUniformCPU(long lseed, int N):
     galsim::BaseDeviate(lseed), buf_len(N), buf_ptr(N) {}
-
+// NOTE: initialize buf_ptr to N so that we're calling fill_buff on the first
+// time generate1() is called
 
 void ConusUniformCPU::fill_buff() {
 
@@ -24,4 +25,8 @@ double ConusUniformCPU::generate1() {
     if (buf_ptr < buf_len) return buf.get()[buf_ptr];
 
     fill_buff();
+
+    // Need to try again after buffer has been filled (I know this pattern is
+    // unsafe)
+    return generate1();
 }
