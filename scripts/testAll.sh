fac2a7bca6328c844b5e4e088e0c93a888986f01
Yunsong Wang <yunsongwang@lbl.gov>
2020-03-09 07:43:15 -0700

    Use absolute path in the test script

--- scripts/testAll.sh
+++ scripts/testAll.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-
+CURDIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 for f in test_cpu.ex test_gpu.ex test_gpu_nvtx.ex profile_gpu.ex
 do
-    srun ../$f 1024
+  srun ${CURDIR}/../$f 1024
 done
