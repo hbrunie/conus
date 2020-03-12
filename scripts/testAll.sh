93a1ace3f407bbcda241587e9cd984623b711d20
Yunsong Wang <yunsongwang@lbl.gov>
2020-03-12 12:03:24 -0700

    Minor update: compact absolute path in script

--- scripts/testAll.sh
+++ scripts/testAll.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-CURDIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+CURDIR=$(dirname ${BASH_SOURCE[0]})
 for f in test_cpu.ex test_gpu.ex test_gpu_nvtx.ex profile_gpu.ex
 do
   srun ${CURDIR}/../$f 1024
